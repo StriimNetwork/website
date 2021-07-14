320da8f8df9014ce6584de55839508c1c9ffebd0
6100m <77129415+6100m@users.noreply.github.com>
2021-07-13 18:19:40 -0600

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,34 +1,26 @@
-r(function(){initalizemii()});
+r(function(){initalizemii(););
 function initalizemii(){
-    var s = document.createElement("script"); 
-    s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"; 
-    s.onload = function(e){initalizeotherscripts()};   
-    document.head.appendChild(s);
-    const seed = initalizeotherscripts()
-    return initalizeothersripts(seed)
-  };
-  function initalizeotherscripts(seed){
-    $.getScript('apis/vidapi/000/vidapi.js', function() {
-      console.log("apis/vidapi/000/vidapi.js was loaded......")
-      $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js', function() {
-        console.log("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js was loaded......")
-      });
-    });
-    return initalizeprimarydata(seed); 
-  };
-  function initalizeprimarydata(seed){
-    const primarydata = "<link rel=\"stylesheet\" href=\"multiplexers/001/css/multiplexmii.css\" type=\"text/css\">\n"
-    $('body').append(primarydata);
-    const secondarydata = "<link rel=\"shortcut icon\" type=\"image/jpeg\" href=\"assets/images/favicon.jpg\">\n"
-    $('body').append(secondarydata);
-    return console.log("multiplexers/001/css/multiplexmii.css was loaded");
-  }
-  r(function() {
-    docReady(function() {
-      return initalizemii()
-    });
-  return document.title = "Kirby TV Channel Revival - KTCR"
+  var s = document.createElement(\"script\");
+  s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+  s.onload = function(e){initalizeotherscripts()};
+  document.head.appendChild(s);
+  document.title = "Kirby TV Channel Revival - KTCR";
+  return Math.random().toString(36).substring(2);
+};
+function initalizeotherscripts(){
+  $.getScript('apis/vidapi/000/vidapi.js', function() {
+    console.log("apis/vidapi/000/vidapi.js was loaded")
+});
+  $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js', function() {
+    console.log("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js was loaded")
   });
-  function r(f) {
-          /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f()
-  }
+  document.title = "XConnect24"
+  initalizethecss();
+  return console.log("multiplexers/001/css/multiplexmii.css was loaded");
+};
+function initalizethecss(){
+  return $('body').append('<link rel="stylesheet" href="multiplexers/001/css/multiplexmii.css" type="text/css"></div></body><div class="d" id="sessionseed" hidden>'+Math.random().toString(36).substring(2)+'</div></html>');
+} 
+function r(f) {
+  /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f()
+}
